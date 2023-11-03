72df360085b9c0c47c9f432ea16ca6cdcaf7b534
Маркова Дарья Александровна <markovada@pgk.ru>
2023-11-03 17:47:01 +0300

    initialization of empty project

---
+++ index.js
@@ -0,0 +1,11 @@
+import {
+  Camera,
+  Material,
+  Texture,
+} from 'three';
+
+import { OrbitControls } from "OrbitControls";
+import { GLTFLoader } from "GFTLoader";
+
+
+console.log("Loaded end")
